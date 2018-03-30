52a2f8b4960cca29c4c875482758b5ae1adadf1b
Kwang-Hun Ji <>
2018-03-30 09:50:39 +0900

    ㅇ

--- a/wp-content/themes/pinnacle/single-portfolio.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); 
-
-	get_template_part('templates/content', 'singleportfolio');
-				
-				get_sidebar(); ?>
-      		</div><!-- /.row-->
-    	</div><!-- /.content -->
-  	</div><!-- /.wrap -->
-  	<?php get_footer(); ?>
--- a/wp-content/themes/pinnacle/single.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); 
-
- 	get_template_part('templates/content', 'single');
- 	
-			 get_sidebar(); ?>
-	    	</div><!-- /.row-->
-		</div><!-- /.content -->
-	</div><!-- /.wrap -->
-	<?php get_footer(); ?>
